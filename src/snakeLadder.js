6ea034228401d0844675def0a9f9f09814287cc7
JasKaur96 <jaspreetflora7@gmail.com>
2021-04-19 20:41:58 +0530

    REFACTOR: Repeat till player reaches 100.

--- src/snakeLadder.js
+++ src/snakeLadder.js
@@ -11,27 +11,37 @@ class SnakeAndLadder{
 
     play(){
         console.log("Game Starts!\nYou are at position :" + this.start);
-        dice = roll.diceRoll();
-        option = roll.optionCheck();
-        console.log("\nDice Rolled : " +dice);
-        switch(option){
-            case 1: 
-                playerPosition += dice; 
-                console.log("Yeah! You got a Ladder. ");
-                break;
-            case 2:
-                playerPosition -= dice;                
-                console.log("Oh No! Snake bit you. ");
-                break;
-            case 3:               
-                console.log("Pass the chance. ");
-                break;
-        }
-        return "Your position is : " +playerPosition;
+          while (playerPosition <= 100) {
+            dice = roll.diceRoll();
+            option = roll.optionCheck();
+            console.log("\nDice Rolled : " +dice);
+     
+            switch(option){
+                case 1: 
+                    playerPosition += dice; 
+                    console.log("Yeah! You got a Ladder. ");
+                    break;
+                case 2:
+                    if(playerPosition >= 0){
+                        playerPosition -= dice;                
+                        console.log("Oh No! Snake bit you. ");
+                        break;
+                    }
+                    else{
+                        playerPosition = 0;                
+                        console.log("Oh No! Player at position Zero. ");
+                        break;
+                    }
+                        
+                case 3:               
+                    console.log("Pass the chance. ");
+                    break;
+            }
+            console.log("Your position is : " +playerPosition);
               
+        }
+        return playerPosition;
     }
-
-
 }
 
 module.exports = {SnakeAndLadder} 
